b7cb8e491d11177b9db322373f85eac8156c9a63
Guilherme Ventura de Souza <venturalp@gmail.com>
2019-04-03 08:27:55 -0300

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import { addRecipe } from '../actions'
 
 class App extends Component {
+
+  state = {
+    calendar: null
+  }
+
+  submitFood = () => {    
+     
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        <input type='text' ref={(input)=> this.input = input} placeholder='Monday breakfast'/>
+        <button onClick={this.submitFood}>Submit</button>
+
+        <pre>
+          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
+        </pre>
       </div>
     )
   }
