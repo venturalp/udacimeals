117613c17d4a781b30f4dab108662199b7e8634a
Guilherme Ventura de Souza <venturalp@gmail.com>
2019-04-22 07:43:50 -0300

    connect

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { addRecipe } from '../actions'
+import  { connect } from 'react-redux'
 
 class App extends Component {
 
@@ -12,7 +13,8 @@ class App extends Component {
   }
 
   render() {
-    return (
+    console.log(this.props)
+    return (      
       <div>
         <input type='text' ref={(input)=> this.input = input} placeholder='Monday breakfast'/>
         <button onClick={this.submitFood}>Submit</button>
@@ -25,4 +27,25 @@ class App extends Component {
   }
 }
 
-export default App
+function mapStateToProps (calendar) {
+  const dayOrder = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+  ]
+  return {
+    calendar: dayOrder.map(day => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal] ? calendar[day][meal] : 'sem receita'
+        return meals
+      }, {})
+    }))
+  }
+}
+
+export default connect(mapStateToProps)(App)
