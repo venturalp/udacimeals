8dad9187fb00f339a9d601f126fbd2b53fddaf56
Guilherme Ventura de Souza <venturalp@gmail.com>
2019-05-08 08:10:50 -0300

    search and foodlist

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import reducer from './reducers'
+// import logger from 'redux-logger'
 import { Provider } from 'react-redux'
+
+const logger = store => next => action => {
+    console.group(action.type)
+    console.info('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd(action.type)
+    return result
+}
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancer(
+        applyMiddleware(logger)
+    ),
 )
-
 
 ReactDOM.render(<Provider store={store}>
                     <App />
